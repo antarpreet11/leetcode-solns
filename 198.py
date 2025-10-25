22adc8252687c05f4d0de54e3616c44c9089fa20
Antarpreet Singh <as2khals@uwaterloo.ca>
2025-10-25 16:00:23 -0400

    198

---
+++ 198.py
@@ -0,0 +1,11 @@
+#Time - O(n), Space - O(1)
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        one, two = 0, nums[0]
+
+        for i in range(1, len(nums)):
+            temp = max((one + nums[i]), two)
+            one = two
+            two = temp
+
+        return two
