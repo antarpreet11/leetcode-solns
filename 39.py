8708c959a4b3dc7af51818e23e69a216efffbc60
Antarpreet Singh <as2khals@uwaterloo.ca>
2025-10-27 02:41:56 -0400

    39

---
+++ 39.py
@@ -0,0 +1,19 @@
+# Time - O(2^(t/m)), Space - O(t/m), t = target m = min(nums)
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+
+        def dfs(i: int, curr: List[int], total: int) -> None:
+            if total == target:
+                res.append(curr.copy())
+                return
+            if i >= len(candidates) or total > target:
+                return
+            
+            curr.append(candidates[i])
+            dfs(i, curr, total + candidates[i])
+            curr.pop()
+            dfs(i + 1, curr, total)
+        
+        dfs(0, [], 0)
+        return res
