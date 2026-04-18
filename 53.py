80dbcb66b359abca8820ababf90cbbfce48514ad
Antarpreet Singh <as2khals@uwaterloo.ca>
2026-04-18 17:53:15 -0400

    maximum subarray

---
+++ 53.py
@@ -0,0 +1,13 @@
+# Time - O(n), Space - O(n) 
+# Kadane's algorithm
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        maxSum = nums[0]
+        currSum = 0
+
+        for i in range(len(nums)):
+            if currSum < 0:
+                currSum = 0
+            currSum = currSum + nums[i]
+            maxSum = max(maxSum, currSum)
+        return maxSum
