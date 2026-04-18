5d06f84f9da07668cc5264e65db0ce32cd904e03
Antarpreet Singh <as2khals@uwaterloo.ca>
2026-04-18 19:56:03 -0400

    two sum

---
+++ 1.py
@@ -0,0 +1,9 @@
+# Time - O(n), Space O(n) 
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        numMap = {}
+
+        for i, n in enumerate(nums):
+            if (target - n) in numMap:
+                return [numMap[target - n], i]
+            numMap[n] = i
