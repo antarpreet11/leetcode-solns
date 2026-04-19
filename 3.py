a2a53452e0d55d762ade4dc43e3ad63f554060b1
Antarpreet Singh <as2khals@uwaterloo.ca>
2026-04-18 21:30:19 -0400

    minor update

--- 3.py
+++ 3.py
@@ -1,18 +1,16 @@
 # Time - O(n), Space - O(m), m - num of unique chars
-from collections import defaultdict
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        numChars = defaultdict(int)
-        l, r = 0, 0
-        maxL = 0
+        currSet = set()
+        maxLen = 0
 
-        while r < len(s):
-            numChars[s[r]] += 1
+        l = 0
 
-            while numChars[s[r]] > 1 and l <= r:
-                numChars[s[l]] -= 1
-                l += 1
-            maxL = max(maxL, r - l + 1)
-            r += 1
-        
-        return maxL
+        for r in range(len(s)):
+            while s[r] in currSet:
+                currSet.remove(s[l])
+                l += 1    
+
+            currSet.add(s[r])
+            maxLen = max(maxLen, r - l + 1)
+        return maxLen
