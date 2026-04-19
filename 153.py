50841f72f1abe3bee160dc3999f38b23ce356aea
Antarpreet Singh <as2khals@uwaterloo.ca>
2026-04-19 02:23:33 -0400

    minumum in rotated sorted array - lower bound

--- 153.py
+++ 153.py
@@ -17,3 +17,17 @@ class Solution:
             else:
                 j = k - 1 
         return res
+    
+# Binary Search, Time - O(logN), Space - O(1)
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        l, r = 0, len(nums) - 1
+
+        while l < r:
+            m = l + ((r - l) // 2)
+
+            if nums[m] < nums[r]:
+                r = m
+            else:
+                l = m + 1
+        return nums[l]
