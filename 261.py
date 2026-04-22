a305b859501bee34106d44405ce5c4a3cb666def
Antarpreet Singh <as2khals@uwaterloo.ca>
2026-04-22 10:21:02 -0400

    graph valid tree

---
+++ 261.py
@@ -0,0 +1,28 @@
+# Time - O(V + E), Space - O(V + E)
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        
+        if len(edges) != n-1:
+            return False
+        
+        edgeMap = [[] for _ in range(n)]
+        visit = set()
+
+        for a, b in edges:
+            edgeMap[a].append(b)
+            edgeMap[b].append(a)
+        
+        def dfs(node, prev):
+            if node in visit:
+                return False
+            
+            visit.add(node)
+
+            for edge in edgeMap[node]:
+                if edge == prev:
+                    continue
+                if not dfs(edge, node):
+                    return False
+            return True
+
+        return dfs(0, 0) and len(visit) == n
